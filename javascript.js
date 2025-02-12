3f9637e8bbe24d56f3c35bda7d23cd4902c77d7a
Keith Estrella <keithestrella@gmail.com>
2025-02-12 03:10:25 -0500

    Add playRound function

--- javascript.js
+++ javascript.js
@@ -27,3 +27,45 @@ function getHumanChoice(userInput){
 }
 
 //console.log(getHumanChoice(prompt("Choose! Rock, paper or scissors?")));
+let playerScore = 0;
+let computerScore = 0;
+
+
+function playRound(humanChoice,computerChoice){
+    if(humanChoice == "scissors"){
+        if(computerChoice == "paper"){
+            playerScore++;
+            return console.log("You win! Scissors beats paper! :D");
+        }else if(computerChoice == "rock"){
+            computerScore++;
+            return console.log("You lose! Rock beats scissors! D:");
+        }else{
+            return console.log("It's a draw!")
+        }
+    }else if(humanChoice == "paper"){
+        if(computerChoice == "rock"){
+            playerScore++;
+            return console.log("You win! Paper beats rock! :D");
+        }else if(computerChoice == "scissors"){
+            computerScore++;
+            return console.log("You lose! Scissors beats paper! D:");
+        }else{
+            return console.log("It's a draw!")
+        }
+    }else if(humanChoice == "rock"){
+        if(computerChoice == "scissors"){
+            playerScore++;
+            return console.log("You win! Rock beats scissors! :D");
+        }else if(computerChoice == "paper"){
+            computerScore++;
+            return console.log("You lose! Paper beats rock! D:");
+        }else{
+            return console.log("It's a draw!")
+        }
+    }else{
+        return console.log("Erm... What the scallop...");
+    }
+}
+
+console.log(playRound(getHumanChoice(prompt("Choose! Rock, paper or scissors?")),getComputerChoice()));
+console.log("Current scores\n" + "Player: " + playerScore + "  |  " + "Computer: " + computerScore);
