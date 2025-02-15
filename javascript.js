cd3f862fb9f84100db137bec39f854f4fa7b0bcb
Keith Estrella <keithestrella@gmail.com>
2025-02-15 00:29:22 -0500

    Introduce UI

--- javascript.js
+++ javascript.js
@@ -11,8 +11,6 @@ function getComputerChoice(){
     }
 }
 
-//console.log(getComputerChoice() + " and " + getComputerChoice() + " and " + getComputerChoice());
-
 function getHumanChoice(){
     let userInput = prompt("Choose! Rock, paper or scissors?");
     userInput = String(userInput).toLowerCase();
@@ -28,103 +26,58 @@ function getHumanChoice(){
     }
 }
 
-//console.log(getHumanChoice(prompt("Choose! Rock, paper or scissors?")));
-// let playerScore = 0;
-// let computerScore = 0;
-
-
-// function playRound(humanChoice,computerChoice){
-//     if(humanChoice == "scissors"){
-//         if(computerChoice == "paper"){
-//             playerScore++;
-//             return console.log("You win! Scissors beats paper! :D");
-//         }else if(computerChoice == "rock"){
-//             computerScore++;
-//             return console.log("You lose! Rock beats scissors! D:");
-//         }else{
-//             return console.log("It's a draw!")
-//         }
-//     }else if(humanChoice == "paper"){
-//         if(computerChoice == "rock"){
-//             playerScore++;
-//             return console.log("You win! Paper beats rock! :D");
-//         }else if(computerChoice == "scissors"){
-//             computerScore++;
-//             return console.log("You lose! Scissors beats paper! D:");
-//         }else{
-//             return console.log("It's a draw!")
-//         }
-//     }else if(humanChoice == "rock"){
-//         if(computerChoice == "scissors"){
-//             playerScore++;
-//             return console.log("You win! Rock beats scissors! :D");
-//         }else if(computerChoice == "paper"){
-//             computerScore++;
-//             return console.log("You lose! Paper beats rock! D:");
-//         }else{
-//             return console.log("It's a draw!")
-//         }
-//     }else{
-//         return console.log("Erm... What the scallop...");
-//     }
-// }
-
-// console.log(playRound(getHumanChoice(),getComputerChoice()));
-// console.log("Current scores\n" + "Player: " + playerScore + "  |  " + "Computer: " + computerScore);
-
-function playGame(){
-    let playerScore = 0;
-    let computerScore = 0;
-
-    function playRound(humanChoice,computerChoice){
-        if(humanChoice == "scissors"){
-            if(computerChoice == "paper"){
-                playerScore++;
-                return console.log("You win! Scissors beats paper! :D");
-            }else if(computerChoice == "rock"){
-                computerScore++;
-                return console.log("You lose! Rock beats scissors! D:");
-            }else{
-                return console.log("It's a draw!")
-            }
-        }else if(humanChoice == "paper"){
-            if(computerChoice == "rock"){
-                playerScore++;
-                return console.log("You win! Paper beats rock! :D");
-            }else if(computerChoice == "scissors"){
-                computerScore++;
-                return console.log("You lose! Scissors beats paper! D:");
-            }else{
-                return console.log("It's a draw!")
-            }
-        }else if(humanChoice == "rock"){
-            if(computerChoice == "scissors"){
-                playerScore++;
-                return console.log("You win! Rock beats scissors! :D");
-            }else if(computerChoice == "paper"){
-                computerScore++;
-                return console.log("You lose! Paper beats rock! D:");
-            }else{
-                return console.log("It's a draw!")
-            }
+function playRound(humanChoice,computerChoice){
+    if(humanChoice == "scissors"){
+        if(computerChoice == "paper"){
+            playerScore++;
+            return console.log("You win! Scissors beats paper! :D");
+        }else if(computerChoice == "rock"){
+            computerScore++;
+            return console.log("You lose! Rock beats scissors! D:");
         }else{
-            return console.log("Erm... What the scallop...");
+            return console.log("It's a draw!")
         }
-    }
-
-    for(let i = 0; i < 5; i++){
-        console.log("Round: " + (i + 1));
-        playRound(getHumanChoice(),getComputerChoice());
-        console.log("Current scores\n" + "Player: " + playerScore + "  |  " + "Computer: " + computerScore);
-    }
-
-    if(playerScore > computerScore){
-        console.log("You did it you win! :DDD");
-    }else if(computerScore > playerScore){
-        console.log("Oh no! You lost! D:");
+    }else if(humanChoice == "paper"){
+        if(computerChoice == "rock"){
+            playerScore++;
+            return console.log("You win! Paper beats rock! :D");
+        }else if(computerChoice == "scissors"){
+            computerScore++;
+            return console.log("You lose! Scissors beats paper! D:");
+        }else{
+            return console.log("It's a draw!")
+        }
+    }else if(humanChoice == "rock"){
+        if(computerChoice == "scissors"){
+            playerScore++;
+            return console.log("You win! Rock beats scissors! :D");
+        }else if(computerChoice == "paper"){
+            computerScore++;
+            return console.log("You lose! Paper beats rock! D:");
+        }else{
+            return console.log("It's a draw!")
+        }
     }else{
-        console.log("Oh dear... It's a draw! :o");
+        return console.log("Erm... What the scallop...");
     }
 }
 
-playGame();
+let playerScore = 0;
+let computerScore = 0;
+
+const scoreboard = document.querySelector("#scoreboard");
+const roundResults = document.querySelector("#roundResults");
+const playerChoices = document.querySelector("#playerChoices");
+
+const score = document.createElement("div");
+score.classList.add("score");
+score.textContent = "Player Wins: " + playerScore + " --- Computer Wins: " + computerScore;
+scoreboard.appendChild(score);
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        playRound(button.id,getComputerChoice())
+    });
+});
